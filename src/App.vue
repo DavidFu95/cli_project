664ba4a9c3b25310c10dac3bfdbadd54a3d0aea0
DavidFu95 <85677992+DavidFu95@users.noreply.github.com>
2021-09-26 18:08:16 +0800

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,20 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <router-view> </router-view>
+    {{ name }}
   </div>
-  <router-view/>
-    <button type="button" class="btn btn-primary">Primary</button>
-    <button type="button" class="btn btn-secondary">Secondary</button>
-    <button type="button" class="btn btn-success">Success</button>
-    <button type="button" class="btn btn-danger">Danger</button>
-    <button type="button" class="btn btn-warning">Warning</button>
-    <button type="button" class="btn btn-info">Info</button>
-    <button type="button" class="btn btn-light">Light</button>
-    <button type="button" class="btn btn-dark">Dark</button>
-
-    <button type="button" class="btn btn-link">Link</button>    
-
 </template>
 
-<style lang="scss"> 
-@import 'bootstrap'
+<style lang="scss">
+@import 'bootstrap';
 </style>
+<script>
+export default {
+  data () {
+    return {
+      name: process.env.VUE_APP_NAME
+    }
+  }
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Card></Card>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Card from '@/components/Card.vue'
+
+export default {
+  components: {
+    Card
+  }
+}
+</script>
